837743758ad100a9479be0cdd092b85eb199da51
Jean Bahnik <jeanbahnik@gmail.com>
2017-03-23 22:48:33 -0700

    04-FILL-wip

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,39 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  @KBD
+  D = M
+  @addr
+  M = D
+  @FILL
+  D; JNE
+  @LOOP
+  0;JMP
+
+(FILL)
+  @SCREEN
+  D=A
+  @addr
+  M=D
+  (FILLLOOP)
+    @SCREEN
+    M=-1
+    @addr
+    M = M + 1
+    @FILLLOOP
+    0;JUMP
+  @END
+  0;JMP
+
+// END the software
+(END)
+  @END
+  0;JMP
